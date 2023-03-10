d7b8f2ecf3349e2ac2250a79b708bc1ab1a904a9
Jorge Alexandre <jorgeaamsantos@gmail.com>
2023-03-10 18:21:10 -0300

    ajuste de usuario repositorio

--- src/views/UsuarioRepositorios/UsuarioRepositorios.vue
+++ src/views/UsuarioRepositorios/UsuarioRepositorios.vue
@@ -1,11 +1,26 @@
 <template>
     <section class="container_UsuarioDetalhe">
-
+        <div class="usuario_info">
+            <img src="" alt="Foto Usuario">
+        </div>
+        <div class="lista_repositorios">
+            <CardResultado />
+            <ButtonVerMais />
+        </div>
     </section>
 </template>
 
 <script>
+import ButtonVerMais from '../../components/ButtonVerMais/ButtonVerMais.vue'
+import CardResultado from '../../components/CardResultado/CardResultado.vue'
+
 export default {
-    name: 'UsuarioRepositorios'
+    name: 'UsuarioRepositorios',
+    components: {
+        ButtonVerMais,
+        CardResultado
+    }
 }
 </script>
+
+<style src="./styles.scss" lang="scss" scoped />
